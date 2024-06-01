c03f0a22995b7779c6c4d37a818cdc7343ce2ee1
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-06-01 14:34:58 +0530

    Create Score_of_a_string.cpp

---
+++ 3110. Score of a String/Score_of_a_string.cpp
@@ -0,0 +1,15 @@
+/*
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+Return the score of s.
+*/
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score = 0;
+        for(int i=0;i <s.length() - 1; ++i){
+            score += abs(s[i] - s[i+1]);
+        }
+        return score;
+    }
+};
