2c301315aa9667922916956d78334ec541818d4f
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-06-06 15:57:06 +0530

    Create Hand-of-Straights.cpp

---
+++ Medium/846. Hand of Straights/Hand-of-Straights.cpp
@@ -0,0 +1,41 @@
+/*
+Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
+
+Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
+Example 1:
+
+Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+Output: true
+Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
+Example 2:
+
+Input: hand = [1,2,3,4,5], groupSize = 4
+Output: false
+Explanation: Alice's hand can not be rearranged into groups of 4.
+
+*/
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+    if(hand.size()%groupSize!=0) return 0;
+    map<int,int> mp;
+    for(auto it:hand){
+        mp[it]++;
+    }
+    while(mp.size()){
+        int p=mp.begin()->first;
+        for(int i=0;i<groupSize;i++){
+            if(mp[p+i]>0){
+                mp[p+i]--;
+                if(mp[p+i]==0){
+                    mp.erase(p+i);
+                }
+            } else{
+                return 0;
+            }
+        }
+    }
+    return 1;
+    }
+};
