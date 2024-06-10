e385c873dc904e8f3feebd14402ef9adecda8d10
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-06-10 20:32:18 +0530

    Update Clear-Digits.cpp

--- Easy/3174. Clear Digits/Clear-Digits.cpp
+++ Easy/3174. Clear Digits/Clear-Digits.cpp
@@ -23,3 +23,31 @@ First, we apply the operation on s[2], and s becomes "c4".
 
 Then we apply the operation on s[1], and s becomes "".
 */
+class Solution {
+public:
+    string clearDigits(string s) {
+        while (true) {
+            bool digitFound = false;
+            for (int i = 0; i < s.length(); ++i) {
+                if (isdigit(s[i])) {
+                    digitFound = true;
+                    int j = i - 1;
+                    while (j >= 0 && isdigit(s[j])) {
+                        j--;
+                    }
+                    if (j >= 0) {
+                        s.erase(i, 1);  
+                        s.erase(j, 1);  
+                    } else {
+                        s.erase(i, 1);  
+                    }
+                    break; 
+                }
+            }
+            if (!digitFound) break; 
+        }
+        return s;
+    }
+};
+
+
