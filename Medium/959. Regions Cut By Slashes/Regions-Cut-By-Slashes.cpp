bc98716de7adfce3c7fcd7126d5c8accb3c331b0
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-08-10 21:25:06 +0530

    Create Regions-Cut-By-Slashes.cpp

---
+++ Medium/959. Regions Cut By Slashes/Regions-Cut-By-Slashes.cpp
@@ -0,0 +1,80 @@
+/*
+An n x n grid is composed of 1 x 1 squares where each 1 x 1 square consists of a '/', '\', or blank space ' '. These characters divide the square into contiguous regions.
+
+Given the grid grid represented as a string array, return the number of regions.
+
+Note that backslash characters are escaped, so a '\' is represented as '\\'.
+
+ 
+
+Example 1:
+
+
+Input: grid = [" /","/ "]
+Output: 2
+Example 2:
+
+
+Input: grid = [" /","  "]
+Output: 1
+Example 3:
+
+
+Input: grid = ["/\\","\\/"]
+Output: 5
+Explanation: Recall that because \ characters are escaped, "\\/" refers to \/, and "/\\" refers to /\.
+ 
+*/
+
+class Solution {
+public:
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+    void numberOfIslandsDFS(vector<vector<int>>& matrix, int i, int j) {
+        if(i < 0 || i >= matrix.size() || j < 0 || j >= matrix[0].size() || matrix[i][j] == 1)
+            return;
+        
+        matrix[i][j] = 1; //mark visited
+
+        for(const auto& dir : directions) {
+            int new_i = i + dir[0];
+            int new_j = j + dir[1];
+            numberOfIslandsDFS(matrix, new_i, new_j);
+        }
+    }
+
+    int regionsBySlashes(vector<string>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+        vector<vector<int>> matrix(n * 3, vector<int>(m * 3, 0));
+        
+        // Populate the matrix based on '/' and '\'
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+                if(grid[i][j] == '/') {
+                    matrix[i * 3][j * 3 + 2] = 1;
+                    matrix[i * 3 + 1][j * 3 + 1] = 1;
+                    matrix[i * 3 + 2][j * 3] = 1;
+                } else if(grid[i][j] == '\\') {
+                    matrix[i * 3][j * 3] = 1;
+                    matrix[i * 3 + 1][j * 3 + 1] = 1;
+                    matrix[i * 3 + 2][j * 3 + 2] = 1;
+                }
+            }
+        }
+
+        // Perform DFS to count regions
+        for(int i = 0; i < matrix.size(); ++i) {
+            for(int j = 0; j < matrix[0].size(); ++j) {
+                if(matrix[i][j] == 0) {
+                    numberOfIslandsDFS(matrix, i, j);
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
