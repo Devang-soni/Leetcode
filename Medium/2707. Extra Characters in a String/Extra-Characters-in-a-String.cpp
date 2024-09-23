4efebf866402328065d7e98f5230e3676fd9ae92
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-09-23 14:37:38 +0530

    Create Extra-Characters-in-a-String.cpp

---
+++ Medium/2707. Extra Characters in a String/Extra-Characters-in-a-String.cpp
@@ -0,0 +1,53 @@
+/*
+You are given a 0-indexed string s and a dictionary of words dictionary. You have to break s into one or more non-overlapping substrings such that each substring is present in dictionary. There may be some extra characters in s which are not present in any of the substrings.
+
+Return the minimum number of extra characters left over if you break up s optimally.
+
+ 
+
+Example 1:
+
+Input: s = "leetscode", dictionary = ["leet","code","leetcode"]
+Output: 1
+Explanation: We can break s in two substrings: "leet" from index 0 to 3 and "code" from index 5 to 8. There is only 1 unused character (at index 4), so we return 1.
+
+Example 2:
+
+Input: s = "sayhelloworld", dictionary = ["hello","world"]
+Output: 3
+Explanation: We can break s in two substrings: "hello" from index 3 to 7 and "world" from index 8 to 12. The characters at indices 0, 1, 2 are not used in any substring and thus are considered as extra characters. Hence, we return 3.
+
+*/
+
+class Solution {
+public:
+    int solve(string& s, unordered_map<string, int>&mp, vector<int>&dp, int index)
+    {
+        if (index >= s.size()) return 0;
+        if (dp[index] != -1) return dp[index];
+        
+        string currStr = "";
+        int minExtra = s.size();
+        for (int cutIdx = index; cutIdx < s.size(); cutIdx++)
+        {
+            currStr.push_back(s[cutIdx]);
+        
+            int currExtra = (mp.count(currStr))? 0 : currStr.size();
+            int nextExtra = solve(s, mp, dp, cutIdx + 1);
+            int totalExtra = currExtra + nextExtra;
+            
+            minExtra = min(minExtra, totalExtra);
+        }
+        return dp[index] = minExtra;
+    }
+    int minExtraChar(string s, vector<string>& dictionary) 
+    {
+        vector<int>dp(s.size(), -1);
+        unordered_map<string, int>mp;
+        for (string& word : dictionary) mp[word]++;
+        
+        int ans = solve(s, mp, dp, 0);
+        return ans;
+        
+    }
+};
